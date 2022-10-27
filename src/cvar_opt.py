0b61e270f274f386131a09f5cb2c892b3a926d6d
gscriva <giuseppe.scriva@unicam.it>
2022-10-27 18:52:56 +0200

    Save results as json

--- src/cvar_opt.py
+++ src/cvar_opt.py
@@ -1,6 +1,8 @@
 from math import pi
 from pathlib import Path
 from typing import List
+import json
+from json import JSONEncoder
 
 import numpy as np
 from qiskit.providers.aer import QasmSimulator
@@ -14,6 +16,8 @@ from src.utils.vqe import create_circ, obj_func, qc_eval
 SIMULATOR = QasmSimulator()
 # define specific optimizer
 METHOD = "COBYLA"
+# initial points number
+NUM_INIT = 10
 # define a ferro ising model
 # with uniform external field
 # TODO J and h could also be np.ndarray
@@ -49,8 +53,9 @@ def cvar_opt(
         adja_dict[(i, i + 1)] = -J
     # class devoted to set the couplings and get the energy
     ising = IsingModel(qubits, dim=DIM, adja_dict=adja_dict, ext_field=field)
+    min_eng = ising.energy(-np.ones(qubits))
     if verbose:
-        print(f"J:{ising.AdjaDict}, h:{ising.ExtField}")
+        print(f"Ising Model\nJ:{ising.AdjaDict}, h:{ising.ExtField}")
 
     # check if directory exists
     if save_dir is not None:
@@ -60,43 +65,93 @@ def cvar_opt(
 
     for shot in shots:
         for steps in maxiter:
-            # generate initial point
-            # p of the article
-            num_param = qubits * (circ_depth + 1)
-            thetas0 = rng.random(num_param) * pi
-            # create and eval the circuit
-            qc = create_circ(thetas0, qubits, circ_depth)
-            if verbose:
-                qc.draw()
-            counts = qc_eval(qc, SIMULATOR, shot)
-            if verbose:
-                plot_histogram(counts)
-
-            # TODO it has to be an object
-            # eval the loss for the initial point
-            obj_func(
-                thetas0,
-                SIMULATOR,
-                qubits,
-                circ_depth,
-                shot,
-                ising,
-                alpha=alpha,
-                verbose=verbose,
-            )
-
-            # start the optimization
-            res = minimize(
-                obj_func,
-                thetas0,
-                args=(SIMULATOR, qubits, circ_depth, shot, ising, alpha),
-                method=METHOD,
-                options={"maxiter": steps, "disp": True},
-            )
+            # create file to save results
+            filename = f"results_shot{shot}_maxiter{steps}.json"
+            with open(filename, "w") as file:
+                json.dump([], file)
 
             if verbose:
-                # compute and eval the results
-                qc = create_circ(res.x, qubits, circ_depth)
+                print(f"\n\nShot {shot} Maxiter {steps}")
+
+            for _ in range(NUM_INIT):
+                # generate initial point
+                # p of the article
+                num_param = qubits * (circ_depth + 1)
+                thetas0 = rng.random(num_param) * pi
+
+                # read json
+                with open(filename, "r") as file:
+                    data = json.load(file)
+                # create and eval the circuit
+                qc = create_circ(thetas0, qubits, circ_depth)
+                if verbose:
+                    qc.draw()
                 counts = qc_eval(qc, SIMULATOR, shot)
                 if verbose:
                     plot_histogram(counts)
+
+                # TODO it has to be an object
+                # eval the loss for the initial point
+                obj_func(
+                    thetas0,
+                    SIMULATOR,
+                    qubits,
+                    circ_depth,
+                    shot,
+                    ising,
+                    alpha=alpha,
+                    verbose=False,
+                )
+
+                # start the optimization
+                res = minimize(
+                    obj_func,
+                    thetas0,
+                    args=(SIMULATOR, qubits, circ_depth, shot, ising, alpha),
+                    method=METHOD,
+                    options={"maxiter": steps, "disp": False},
+                )
+
+                qc = create_circ(res.x, qubits, circ_depth)
+                counts = qc_eval(qc, SIMULATOR, shot)
+                eng_opt = np.inf
+                sample_opt = np.empty(qubits)
+                for sample in counts.keys():
+                    # cast the sample in np.ndarray
+                    # and in ising notation {+1,-1}
+                    sample_ising = np.asarray(list(sample), dtype=int) * 2 - 1
+                    # compute the energy of the sample
+                    eng = ising.energy(sample_ising)
+                    if eng < eng_opt:
+                        eng_opt = eng
+                        sample_opt = np.copy(sample_ising)
+                if verbose:
+                    print(f"Found minimum {sample_opt} energy {eng_opt}")
+                    if eng_opt == min_eng:
+                        print(f"Global minimum reach!!\n")
+                # save results
+                result = dict(res)
+                result["sample_opt"] = sample_opt
+                result["eng_opt"] = eng_opt
+                result["global__min"] = eng_opt == min_eng
+                # update json
+                data.append(result)
+
+                with open(filename, "w") as file:
+                    json.dump(data, file, cls=NumpyArrayEncoder, indent=4)
+
+                if verbose:
+                    print(result)
+                    if eng_opt == min_eng:
+                        print(f"Global minimum reach!!\n")
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, np.bool_):
+            return bool(obj)
+        if isinstance(obj, np.float64):
+            return float(obj)
+        return JSONEncoder.default(self, obj)
